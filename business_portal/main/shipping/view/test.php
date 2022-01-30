0363fad005421ac0cc9ba30e88f426975682ed6c
vanKvo <80080714+vanKvo@users.noreply.github.com>
2022-01-30 14:26:43 -0500

    Delete business_portal/main/shipping directory

--- business_portal/main/shipping/view/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../../connect.php';
-require_once '../model/shipping_data.php';
-require_once '../model/inventory_data.php';
-
-      $recipient = get_recipient('2', 'Phuong Dang', '5718889900'); 
-      echo "new recipient:" . $recipient[0]['recipient_id'];
-?>
