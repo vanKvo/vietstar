4724035be19636ea1bbe57841923779fb6e1d201
vanKvo <80080714+vanKvo@users.noreply.github.com>
2022-03-16 09:25:15 -0400

    minor change

--- business_portal/main/inventory/navfixed.php
+++ business_portal/main/inventory/navfixed.php
@@ -3,7 +3,7 @@
 			<div class="navbar-header">
 				<a  class="navbar-brand" href="#">Vietstar Shipping</a>	
 				<ul class="nav nav-user pull-right"><!-- navbar-nav -->
-					<li><a href="#"><i class="icon-user icon-large"></i> Welcome <?=$position?>!</a></li>
+					<li><a href="#"><i class="icon-user icon-large"></i> Welcome <?=$name?>!</a></li>
 					<li><a href="../../logout.php"><i class="icon-off icon-large"></i> Log Out</a></li> 
 				</ul>
 			</div>
