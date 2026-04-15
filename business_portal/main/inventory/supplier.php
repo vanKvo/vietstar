81d5002e03dbf4e36fd2a080d9fcf4848426f160
Van <gabong@Vans-MBP.home>
2026-04-15 16:57:26 -0400

    Fixed tabs in Inventory.

--- business_portal/main/inventory/supplier.php
+++ business_portal/main/inventory/supplier.php
@@ -8,45 +8,61 @@ $finalcode=createRandomPassword();
 ?>
 <html>
 <head>
-    <title>Vietstar_Shipping - Suppliers</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../css/lib/bootstrap.css">
-    <link rel="stylesheet" href="../css/lib/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="../css/styles.css">
-    <link rel="stylesheet" type="text/css" href="../css/maindashboard.css">
-    <link rel="stylesheet" type="text/css" href="../css/navbar.css">
-    <style>
-        .sticky { position: fixed; top: 53px; }
-        .top-sticky { position: fixed; top: 0; width: 100%; }
-        .modal-body form { width: 100%; }
-        .modal-body form input { width: 100% !important; margin-bottom: 10px; }
-    </style>
+<title>Vietstar_Shipping</title>
+<link href="css/bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/DT_bootstrap.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link href="css/bootstrap-responsive.css" rel="stylesheet">
+<link href="css/style.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="css/navbar.css" media="screen" rel="stylesheet" type="text/css" />
+<style type="text/css">
+.well li {
+	line-height: 20px;
+	list-style: none;
+	padding-bottom: 10px;
+}
+</style>
+<!--sa poip up-->
+<script src="jeffartagame.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/application.js" type="text/javascript" charset="utf-8"></script>
+<link href="src/facebox.css" media="screen" rel="stylesheet" type="text/css" />
+<script src="lib/jquery.js" type="text/javascript"></script>
+<script src="src/facebox.js" type="text/javascript"></script>
+<script type="text/javascript">
+  jQuery(document).ready(function($) {
+    $('a[rel*=facebox]').facebox({
+      loadingImage : 'src/loading.gif',
+      closeImage   : 'src/closelabel.png'
+    })
+  })
+</script>
 </head>
 
 <body>
 <?php include('navfixed.php');?>
-<nav class="navbar-primary sticky">
-    <a href="#" class="btn-expand-collapse"><span class="glyphicon glyphicon-menu-left"></span></a>
-    <ul class="navbar-primary-menu">
-        <li> <a class="d-flex align-items-center pl-3 text-white text-decoration-none"><span class="fs-4">Inventory</span></a></li>
-        <li><a href="../index.php" class="nav-link text-white"><i class="icon-dashboard icon-2x"></i> Dashboard  </a></li> 
-        <li><a href="products.php" class="nav-link text-white"><i class="icon-list-alt icon-2x"></i> Inventory</a></li>    
-        <li><a href="purchase.php" class="nav-link text-white"><i class="icon-group icon-2x"></i> Store Orders </a> </li>     
-        <li><a href="sales.php?id=cash&invoice=<?php echo $finalcode ?>" class="nav-link text-white"><i class="icon-shopping-cart icon-2x"></i> Sales </a></li>             
-        <li><a href="supplier.php" class="nav-link text-white active"><i class="icon-group icon-2x"></i> Suppliers</a></li> 
-    </ul>             
-</nav>
-
-<div class="main-content">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card mt-4">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    <h4><i class="icon-group"></i> Suppliers</h4>
-                    <button class="btn btn-primary custom-btn ajax-modal-btn" data-url="addsupplier.php" data-title="Add Supplier"><i class="icon-plus-sign"></i> Add Supplier</button>
-                </div>
-                <div class="card-body">
+<div class="container-fluid">
+      <div class="row-fluid">
+	<div class="span2">
+	<div class="well sidebar-nav">
+				<ul class="nav nav-list">
+					<h4>Inventory</span></h4>  
+					<hr>
+					<li><a href="../index.php"><i class="icon-dashboard icon-2x"></i> Dashboard  </a></li> 
+					<li><a href="products.php"><i class="icon-list-alt icon-2x"></i> Inventory</a></li>    
+					<li><a href="purchase.php"><i class="icon-group icon-2x"></i> Store Orders </a> </li>     
+					<li><a href="sales.php?id=cash&invoice=<?php echo $finalcode ?>"><i class="icon-shopping-cart icon-2x"></i> Sales </a></li>             
+					<li class="active"><a href="supplier.php"><i class="icon-group icon-2x"></i> Suppliers</a></li> 
+				</ul>       
+			</div><!--/.well -->
+		</div><!--/span-->
+	<div class="span10">
+	<div class="contentheader">
+			<i class="icon-group"></i> Suppliers
+			</div>
+			<ul class="breadcrumb">
+			<li><a href="../index.php">Dashboard</a></li> /
+			<li class="active">Suppliers</li>
+			</ul>
 
 
 <div style="margin-top: -19px; margin-bottom: 21px;">
@@ -61,8 +77,10 @@ $finalcode=createRandomPassword();
 			</div>
 </div>
 <input type="text" name="filter" style="height:35px; margin-top: -1px;" value="" id="filter" placeholder="Search Supplier..." autocomplete="off" />
-                    <div class="table-responsive">
-                        <table class="table table-bordered table-striped table-hover bg-white" id="resultTable">
+<a rel="facebox" href="addsupplier.php"><Button type="submit" class="btn btn-info" style="float:right; width:230px; height:35px;" /><i class="icon-plus-sign icon-large"></i> Add Supplier</button></a><br><br>
+
+
+<table class="table table-bordered" id="resultTable" data-responsive="table" style="text-align: left;">
 	<thead>
 		<tr>
 			<th> Supplier </th>
@@ -87,8 +105,8 @@ $finalcode=createRandomPassword();
 			<td><?php echo $row['suplier_address']; ?></td>
 			<td><?php echo $row['suplier_contact']; ?></td>
 			<td><?php echo $row['note']; ?></td>
-			<td><button class="btn btn-warning btn-sm ajax-modal-btn" data-url="editsupplier.php?id=<?php echo $row['suplier_id']; ?>" data-title="Edit Supplier"><i class="icon-edit"></i> Edit</button>
-			<a href="#" id="<?php echo $row['suplier_id']; ?>" class="delbutton btn btn-danger btn-sm text-white text-decoration-none" title="Click To Delete"><i class="icon-trash"></i> Delete</a></td>
+			<td><a rel="facebox" href="editsupplier.php?id=<?php echo $row['suplier_id']; ?>"><button class="btn btn-warning btn-mini"><i class="icon-edit"></i> Edit </button></a>
+			<a href="#" id="<?php echo $row['suplier_id']; ?>" class="delbutton" title="Click To Delete"><button class="btn btn-danger btn-mini"><i class="icon-trash"></i> Delete</button></a></td>
 			</tr>
 			<?php
 				}
@@ -96,93 +114,49 @@ $finalcode=createRandomPassword();
 		
 	</tbody>
 </table>
-                    </div><!-- table-responsive -->
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="clearfix"></div>
+</div>
+</div>
 </div>
 
-<!-- Bootstrap AJAX Modal -->
-<div class="modal fade" id="ajaxModal" tabindex="-1" aria-labelledby="ajaxModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="ajaxModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body" id="ajaxModalBody">
-        Loading...
-      </div>
-    </div>
-  </div>
-</div>
+<script src="js/jquery.js"></script>
+  <script type="text/javascript">
+$(function() {
 
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-<script>
-$(document).ready(function() {
-    // Handle AJAX modals
-    $('.ajax-modal-btn').on('click', function(e) {
-        e.preventDefault();
-        var url = $(this).data('url');
-        var title = $(this).data('title');
-        $('#ajaxModalLabel').text(title);
-        $('#ajaxModalBody').html('Loading...');
-        var myModal = new bootstrap.Modal(document.getElementById('ajaxModal'));
-        myModal.show();
 
-        $.get(url, function(data) {
-            $('#ajaxModalBody').html(data);
-        }).fail(function() {
-            $('#ajaxModalBody').html('<div class="alert alert-danger">Error loading content.</div>');
-        });
-    });
+$(".delbutton").click(function(){
 
-    $(".delbutton").click(function(e) {
-        e.preventDefault();
-        var element = $(this);
-        var del_id = element.attr("id");
-        var info = 'id=' + del_id;
-        if(confirm("Are you sure want to delete? There is NO undo!")) {
-            $.ajax({
-                type: "GET",
-                url: "deletesupplier.php",
-                data: info,
-                success: function(){
-                    element.parents(".record").fadeOut("slow", function(){
-                        $(this).remove();
-                    });
-                }
-            });
-        }
-    });
+//Save the link in a variable called element
+var element = $(this);
 
-    // Fix navbar toggle
-    $(".btn-expand-collapse").click(function(e){
-        e.preventDefault();
-        var navbar = $(".navbar-primary");
-        if(navbar.width() > 100){
-            navbar.css("width", "60px");
-            $(".navbar-primary-menu li a span").hide();
-        } else {
-            navbar.css("width", "250px");
-            $(".navbar-primary-menu li a span").show();
-        }
-    });
+//Find the id of the link that was clicked
+var del_id = element.attr("id");
 
-	$("#filter").keyup(function() {
-        var filter = $(this).val().toLowerCase();
-        $("#resultTable tbody tr").each(function () {
-            var rowText = $(this).text().toLowerCase();
-            if (rowText.indexOf(filter) !== -1) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
-        });
-    });
+//Built a url to send
+var info = 'id=' + del_id;
+ if(confirm("Are you sure want to delete? There is NO undo!"))
+		  {
+
+ $.ajax({
+   type: "GET",
+   url: "deletesupplier.php",
+   data: info,
+   success: function(){
+   
+   }
+ });
+         $(this).parents(".record").animate({ backgroundColor: "#fbc7c7" }, "fast")
+		.animate({ opacity: "hide" }, "slow");
+
+ }
+
+return false;
+
+});
+
 });
 </script>
 </body>
+<?php include('footer.php');?>
+
 </html>
